d1fbe4a871e79eeae27aff30c9bdaa20748b817e
s.yashenko <travikubani@mail.ru>
2021-09-30 16:38:35 +0300

    подключился к 1 С

--- app/src/main/java/com/example/order/Server/Retrofit1C.kt
+++ app/src/main/java/com/example/order/Server/Retrofit1C.kt
@@ -1,12 +1,17 @@
 package com.example.order.Server
 
+import com.example.order.BuildConfig
 import com.google.gson.GsonBuilder
+import okhttp3.Credentials
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
+import java.net.PasswordAuthentication
+import kotlin.jvm.Throws
 
 class Retrofit1C {
     private val baseUrl="http://192.168.8.8/YUB_AF_PL/hs/"
@@ -21,6 +26,7 @@ class Retrofit1C {
     }
     private fun createOkHttpClient(interceptor:Interceptor):OkHttpClient{
         val httpClient=OkHttpClient.Builder()
+        httpClient.addInterceptor(AuthInterceptor(BuildConfig.API_USERNAME,BuildConfig.API_PASSWORD))
         httpClient.addInterceptor(interceptor)
         httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         return httpClient.build()
@@ -29,6 +35,19 @@ class Retrofit1C {
     inner class Interceptor1C:Interceptor{
         override fun intercept(chain: Interceptor.Chain): Response {
             return chain.proceed((chain.request()))
+        }
+
+
+
+    }
+    inner class AuthInterceptor(user:String,password:String):Interceptor{
+        private val credentials:String=Credentials.basic(user,password)
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request=chain.request()
+            val authRequest=request.newBuilder()
+                .header("Authorization",credentials).build()
+            return chain.proceed((authRequest))
         }
 
     }
